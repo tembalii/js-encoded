25a07506ed37a9e7e762a6d9bde1b9f6aa19079f
tembalii <159453738+tembalii@users.noreply.github.com>
2025-01-29 13:37:17 +0100

    Create inner.js

---
+++ inner.js
@@ -0,0 +1,17 @@
+<SpecHeading>{change.heading}</SpecHeading>
+<Divider />
+<div className="flex h-full w-full flex-col items-center justify-center gap-3 pt-3 text-base">
+  {submission === "pending" && (
+    <>
+      <span
+        dangerouslySetInnerHTML={{ __html: change.oldLabel }}
+        className="text-primary"
+      />
+      <ChevronDoubleDownIcon className="h-5 w-5 shrink-0 text-primary" />
+      <span
+        dangerouslySetInnerHTML={{ __html: change.newLabel }}
+        className="text-xl font-bold"
+      />
+    </>
+  )}
+</div>
