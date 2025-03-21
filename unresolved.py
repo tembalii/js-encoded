6bebb830e3212423612c4a0356de1f50c56f9800
tembalii <159453738+tembalii@users.noreply.github.com>
2025-03-21 12:10:12 +0100

    Create unresolved.py

---
+++ unresolved.py
@@ -0,0 +1,30 @@
+import os
+
+import paramiko
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/direct/{input}")
+def direct_response(input: str):
+
+    tainted = input
+    rand = os.urandom()
+
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.WarningPolicy())
+    client.connect('localhost', port, username, password)
+
+    # proruleid: tainted-os-command-paramiko-fastapi
+    stdin, stdout, stderr = client.exec_command(f"git clone {tainted}")
+
+    # ok: tainted-os-command-paramiko-fastapi
+    stdin, stdout, stderr = client.exec_command(f"git clone {rand}")
+
+    # ok: tainted-os-command-paramiko-fastapi
+    stdin, stdout, stderr = client.exec_command(f"git clone my-repo")
+
+
+    client.close()
